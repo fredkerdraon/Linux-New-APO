7256780069ab5ac94588666288aca2e5f5908bac
Frederic Kerdraon <frederic.kerdraon@up.coop>
2018-12-24 15:42:28 +0100

    Modified Sql for indexes

--- Sql/Mysql-create.sql
+++ Sql/Mysql-create.sql
@@ -68,18 +68,16 @@ create table cashflows(
 	MyDate datetime,
 	GroupLib varchar(50),
 	Libelle varchar(50),
-	Debit double,
-	Credit double,
+	Debit double(10,2),
+	Credit double(10,2),
 	Category varchar(25),
 	Weekly bool,
 	Monthly bool, 
 	Yearly bool 
 );
 
-ALTER IGNORE TABLE cashflows 
-ADD UNIQUE INDEX idx_cashflows_debit (MyDate, Libelle, Debit);
-ALTER IGNORE TABLE cashflows 
-ADD UNIQUE INDEX idx_cashflows_credit (MyDate, Libelle, Credit);
+ALTER TABLE cashflows ADD UNIQUE INDEX idx_cashflows_debit (MyDate, Libelle, Debit);
+ALTER TABLE cashflows ADD UNIQUE INDEX idx_cashflows_credit (MyDate, Libelle, Credit);
 
 create table balance(
 	MinDate datetime,
